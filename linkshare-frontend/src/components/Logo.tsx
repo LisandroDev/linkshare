d2ac687f64b1442d48dcd315d209b7a8a70a6cfe
lichaa824 <lisandrorubi@gmail.com>
2023-07-25 18:27:58 -0300

    Add logo component

---
+++ linkshare-frontend/src/components/Logo.tsx
@@ -0,0 +1,13 @@
+import LogoSVG from '../assets/logos/logo-no-background.svg'
+
+function Logo(){
+    return(
+        <div>
+
+            <img src={LogoSVG} className='w-44 fill-orange-300' alt="" />
+
+        </div>
+    )
+}
+
+export default Logo
