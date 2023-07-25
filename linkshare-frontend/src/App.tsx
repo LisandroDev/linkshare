141eb82aec0cf0e75c6e33d5e15988f0e72fb66d
lichaa824 <lisandrorubi@gmail.com>
2023-07-25 18:27:48 -0300

    Add landing page components

--- linkshare-frontend/src/App.tsx
+++ linkshare-frontend/src/App.tsx
@@ -1,10 +1,18 @@
-
+import { LazyMotion, domAnimation } from "framer-motion"
 import './App.css'
+import Landing from "./Pages/Landing/Landing.tsx";
+import Navbar from "./components/Navbar.tsx";
+import Footer from "./components/Footer.tsx"
 
 function App() {
 
   return (
     <>
+    <Navbar />
+    <LazyMotion features={domAnimation}>
+    <Landing />
+    </LazyMotion>
+    <Footer />
     </>
   )
 }
