fcc523d565e43bcb63230d53e55814f42984d90d
lichaa824 <lisandrorubi@gmail.com>
2023-08-04 10:15:56 -0300

    Add profile provider to creator

--- linkshare-frontend/src/App.tsx
+++ linkshare-frontend/src/App.tsx
@@ -5,19 +5,24 @@ import Landing from './Pages/Landing/Landing.tsx';
 import Register from './Pages/Register/Register.tsx';
 import Creator from './Pages/Creator/Creator.tsx';
 import Login from './Pages/Login/Login.tsx';
+import { ProfileProvider } from './context/Profile.context.tsx';
 
 
 function App() {
   return (
     <>
-        <LazyMotion features={domAnimation}>
-          <Routes>
-            <Route path="/" element={<Landing />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/creator" element={<Creator />} />
-          </Routes>
-        </LazyMotion>
+      <LazyMotion features={domAnimation}>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/creator" element={
+            <ProfileProvider>
+              <Creator />
+            </ProfileProvider>
+          } />
+        </Routes>
+      </LazyMotion> 
     </>
   );
 }
